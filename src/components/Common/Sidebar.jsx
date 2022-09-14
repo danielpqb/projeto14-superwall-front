11947bc44c689f813d53795118d12142356f0f0f
g-rmc <g.rmc3000@gmail.com>
2022-09-13 21:31:27 -0300

    first commit for sidebar

---
+++ src/components/Common/Sidebar.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+import logo from "../../assets/img/logo.svg";
+
+export default function Sidebar() {
+  return (
+    <Container>
+     Menu Lateral
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  img {
+    width: 70%;
+    padding-bottom: 40px;
+  }
+`;
