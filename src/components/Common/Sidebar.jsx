8667088f7905d90aeda4e4d486617811eaf89ecc
danielpqb <danielpqb@gmail.com>
2022-09-19 12:27:41 -0300

    Bugfix

--- src/components/Common/Sidebar.jsx
+++ src/components/Common/Sidebar.jsx
@@ -6,133 +6,134 @@ import logoBlue from "../../assets/img/logo-blue.svg";
 import UserContext from "../../Context/UserContext";
 
 export default function Sidebar() {
-
   const navigate = useNavigate();
 
   const { setShowSideBar, userData } = useContext(UserContext);
-  const loginMenu = {iconName: 'person-circle', legend: '', route:'' }
+  const loginMenu = { iconName: "person-circle", legend: "", route: "" };
 
-  if (userData.name){
-    loginMenu.legend = 'Bem vinde ' + userData.name;
-    loginMenu.route = '/';
+  if (userData.name) {
+    loginMenu.legend = "Bem-vindo, " + userData.name.split(" ")[0];
+    loginMenu.route = "/";
   } else {
-    loginMenu.legend = 'Entre ou crie sua conta';
-    loginMenu.route = '/login';
+    loginMenu.legend = "Entre ou crie sua conta";
+    loginMenu.route = "/login";
   }
 
   const menuItens = [
     loginMenu,
-    {iconName: 'notifications', legend: 'Promoções', route:'/notifications' },
-    {iconName: 'receipt', legend: 'Compras anteriores', route:'/orders' },
-    {iconName: 'cart', legend: 'Carrinho de compras', route:'/cart' },
+    { iconName: "notifications", legend: "Promoções", route: "/notifications" },
+    { iconName: "receipt", legend: "Compras anteriores", route: "/orders" },
+    { iconName: "cart", legend: "Carrinho de compras", route: "/cart" },
   ];
 
   const sessions = [
-    {title:"Alimentos", route:"/"},
-    {title:"Bebidas", route:"/"},
-    {title:"Higiene", route:"/"},
-    {title:"Produtos de Limpeza", route:"/"},
-    {title:"Farmácia", route:"/"},
-  ]
+    { title: "Alimentos", route: "/" },
+    { title: "Bebidas", route: "/" },
+    { title: "Higiene", route: "/" },
+    { title: "Produtos de Limpeza", route: "/" },
+    { title: "Farmácia", route: "/" },
+  ];
 
-  function Item({item}) {
+  function Item({ item }) {
     return (
-      <StyledItem onClick={() => {
-        setShowSideBar(false);
-        navigate(item.route);
-      }}>
-        <div >
+      <StyledItem
+        onClick={() => {
+          setShowSideBar(false);
+          navigate(item.route);
+        }}
+      >
+        <div>
           <ion-icon name={item.iconName} />
         </div>
 
         <h2>{item.legend}</h2>
       </StyledItem>
-    )
+    );
   }
 
-  function Session({session}) {
+  function Session({ session }) {
     return (
-      <StyledSession onClick={() => {
-        setShowSideBar(false);
-        navigate(session.route);
-      }}>
+      <StyledSession
+        onClick={() => {
+          setShowSideBar(false);
+          navigate(session.route);
+        }}
+      >
         <h2>{session.title}</h2>
       </StyledSession>
-    )
+    );
   }
 
   return (
     <Container>
-
       <SideMenu>
-
-      <SessionHeader>
-          <img src={logoBlue} alt=""/>
+        <SessionHeader>
+          <img src={logoBlue} alt="" />
           <h3>SUPERWALL</h3>
         </SessionHeader>
 
         <MenuItens>
-          {menuItens.map((item, index) => <Item key={index} item={item} />)}
+          {menuItens.map((item, index) => (
+            <Item key={index} item={item} />
+          ))}
         </MenuItens>
 
         <SessionHeader>
-          <img src={logoBlue} alt=""/>
+          <img src={logoBlue} alt="" />
           <h3>ESCOLHA UMA SESSÃO</h3>
         </SessionHeader>
 
         <Sessions>
-          {sessions.map((session, index) => <Session key={index} session={session} />)}
+          {sessions.map((session, index) => (
+            <Session key={index} session={session} />
+          ))}
         </Sessions>
-        
-        
       </SideMenu>
 
-      <DarkTransparency onClick={() => setShowSideBar(false)}/>
-      
+      <DarkTransparency onClick={() => setShowSideBar(false)} />
     </Container>
   );
 }
 
 const Container = styled.div`
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 
-    padding: 70px 0 60px 0;
+  padding: 70px 0 60px 0;
 
-    display: flex;
+  display: flex;
 
-    position: fixed;
-    top: 0;
-    left: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
 
-    z-index: 5;
-
+  z-index: 5;
 `;
 
 const SideMenu = styled.div`
-    height: 100%;
-    width: 70%;
+  height: 100%;
+  width: 70%;
 
-    background-color: white;
-    display: flex;
-    flex-direction: column;
-`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+`;
 
 const DarkTransparency = styled.div`
   height: 100%;
   width: 30%;
 
-  background-color: rgba(0,0,0,0.6);
+  background-color: rgba(0, 0, 0, 0.6);
 
   cursor: pointer;
-`
+`;
 
 const MenuItens = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
-`
+`;
 
 const StyledItem = styled.div`
   width: 100%;
@@ -143,18 +144,18 @@ const StyledItem = styled.div`
   padding-left: 20px;
   cursor: pointer;
 
-  h2{
+  h2 {
     color: var(--azul-base);
     font-weight: 600;
     font-size: 20px;
     margin-left: 18px;
   }
 
-  div{
+  div {
     background: var(--azul-base);
-    height:40px;
-    width:40px;
-    border-radius:50%;
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
   }
 
   ion-icon {
@@ -162,12 +163,12 @@ const StyledItem = styled.div`
   }
 
   &:hover {
-    filter:brightness(1.40);
+    filter: brightness(1.4);
     h2 {
       font-weight: 800;
     }
   }
-`
+`;
 
 const SessionHeader = styled.div`
   height: 80px;
@@ -182,11 +183,11 @@ const SessionHeader = styled.div`
     width: 35px;
   }
 
-  h3{
+  h3 {
     color: var(--azul-base);
-    font-weight:900;
+    font-weight: 900;
   }
-`
+`;
 
 const Sessions = styled.div`
   display: flex;
@@ -194,7 +195,7 @@ const Sessions = styled.div`
   align-items: flex-start;
   justify-content: flex-start;
   padding: 0 20px;
-`
+`;
 
 const StyledSession = styled.div`
   height: 30px;
@@ -203,14 +204,14 @@ const StyledSession = styled.div`
   justify-content: flex-end;
   cursor: pointer;
 
-  h2{
+  h2 {
     color: var(--azul-base);
   }
 
   &:hover {
-    filter:brightness(1.40);
+    filter: brightness(1.4);
     h2 {
       font-weight: 600;
     }
   }
-`
+`;
